fab337a400f93e0c94e2716b0fd72ae1c75c4de5
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-07-31 23:03:16 -0400

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validate :clickbait
+
+  validates :title , presence: true
+  validates :content , length: {minimum: 250}
+  validates :summary , length: { in: 0..250}
+  validates :category , inclusion: ["Fiction" , "Non-Fiction"]
+
+
+  def clickbait
+    valid_arr = [/(Won't Believe)/ , /(Secret)/ , /(Top )+[0-9]/ ,/(Guess)/]
+    res = valid_arr.filter {|regex| regex.match(title)}
+    if res.empty?
+      errors.add(:title , "Not Clickbait-y enough")
+    end
+  end
 end
